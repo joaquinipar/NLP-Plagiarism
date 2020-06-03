daca65459148338be054a7ec2968d8cfdbb07558
Joaquin Ipar <joaquin.ipar@gmail.com>
2020-06-02 23:30:33 -0300

    Renamed plagiarism func to scrape and finished it

--- corpus.py
+++
@@ -1,8 +0,0 @@
-str1 = "The man was walking on the street."
-str2 = "The man was walking around the street"
-
-str = "i was walking between two big dogs"
-str3 = "there was a person walking in the middle of two giant dogs"
-str4 = "No use complaining, no point in singing the blues"
-str5 = "But the president was unclear about the speed and full scope of the actions, and his remarks left many questions unanswered. Stock markets rose after Mr. Trump’s speech in the White House Rose Garden, suggesting that investors had feared the president would take even more draconian steps against China, the world’s second-largest economy."
-str6 = "I'm feeling the pressure I can't break out No one can hear me scream and shout Get out of my face, out of my mind I see your corruption I'm not blind I'll carry the burden and take the strain And when I am done I will make you pay"
